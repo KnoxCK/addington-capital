02ba6d8b7c9565769dc9c59cd4037fcbfcb47f7f
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-11 12:32:32 +0100

    download brochure done but with wrong file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:download_brochure]
 
   def download_brochure
+    send_file "#{Rails.root}/app/assets/AddingtonBrochure.pdf", type: "application/pdf", x_sendfile: true
   end
 end
