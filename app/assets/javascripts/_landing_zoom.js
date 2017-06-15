522e9c0622d8a9a6a538f9fbbbca991ddf1d9351
Clarissa Knox <knox.ck@gmail.com>
2017-06-15 20:45:57 +0100

    arrows & landing

--- app/assets/javascripts/_landing_zoom.js
+++ app/assets/javascripts/_landing_zoom.js
@@ -1,8 +1,7 @@
 $( document ).ready( function() {
-   $('#header-page-down').click( function () { // THIS IS THE DOWN ARROW SELECTOR
-       $( '#Wrapper' ).attr( 'style', 'margin-top: -'+jQuery(window).height()+'px!important;' ); // IMMEDIATELY MOVE THE LOWER CONTENT TO THE TOP OF THE PAGE
-       $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {}); // MOVE THE TOP IMAGE / SECTION OUT OF VIEW IN 0.2 SECONDS
-        
+   $('#header-page-down').click( function (e) { // Put that e in there
+         e.preventDefault(); // This will stop the link from clicking through / doing its thing
+       $( '#Wrapper' ).attr( 'style', 'margin-top: -'+jQuery(window).height()+'px!important;' );
+       $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {});
     });
 } );
-
