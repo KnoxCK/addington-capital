71b8ab8478877caa6cbc18b06254cedde8e1f959
Clarissa Knox <knox.ck@gmail.com>
2017-06-16 15:30:24 +0100

    what we do & press read more

--- app/assets/javascripts/_landing_zoom.js
+++ app/assets/javascripts/_landing_zoom.js
@@ -1,49 +1,31 @@
 
 $( document ).ready( function() {
 
-  $("#header-page-down").click(function(e) {
-    e.preventDefault();
-      $('html,body').animate({
-          scrollTop: $("#press").offset().top - 100},
-          'slow');
-  });
-
   $("#press-page-down").click(function(e) {
     e.preventDefault();
       $('html,body').animate({
           scrollTop: $(".what-we-do-wrapper").offset().top - 100},
           'slow');
   });
-
-  $("#what-we-do-page-down").click(function(e) {
-    e.preventDefault();
-      $('html,body').animate({
-          scrollTop: $(".blue-contact-wrapper").offset().top - 100},
-          'slow');
-  });
-
-  $("#back-to-top").click(function(e) {
-    e.preventDefault();
-      $('html,body').animate({
-          scrollTop: 0},
-          'slow');
-  });
   
-//$( document ).ready( function() {
-  // $('#header-page-down').click( function (e) { 
-    //     e.preventDefault(); 
-      // $( '.blue-value-wrapper' ).attr( 'style', 'margin-top: -'+jQuery(window).height()+'px!important;');
-       //$('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {});
-    //});
-//} );
-
-
-// $(document).ready(function() {
-//    $('#header-page-down').click( function (e) { 
-// 	e.preventDefault();
-// 	Var height = jQuery(window).height(); /
-// 	height = height + $(‘.navbar-addcap’).height(); 
-//        $( '.blue-value-wrapper').attr( 'style', 'margin-top: -'+ height +'px!important;' ); 
-//        $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {}); 
+// $( document ).ready( function() {
+//   $('#header-page-down').click( function (e) { 
+//         e.preventDefault(); 
+//         var height = jQuery(window).height() + $('.navbar-addcap').height();
+//       $( '.blue-value-wrapper' ).attr( 'style', 'margin-top: -'+jQuery(window).height()+'px!important;');
+//        $('#home-main').animate({ "top": -height }, 200, function () {});
 //     });
+// } );
+
+
+   $('#header-page-down').click( function (e) { 
+	e.preventDefault();
+	var height = jQuery(window).height(); 
+	height = height + $('.navbar-addcap').height() - 25; 
+       $( '.blue-value-wrapper').attr( 'style', 'margin-top: -'+ height +'px!important;' ); 
+       $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {}); 
+    });
+
+
+
 });
---
+++ app/assets/javascripts/_links_jump.js
@@ -0,0 +1,22 @@
+$( document ).ready( function() {
+  $("#what-we-do-page-down").click(function(e) {
+    e.preventDefault();
+      $('html,body').animate({
+          scrollTop: $(".blue-contact-wrapper").offset().top - 100},
+          'slow');
+  });
+
+  $("#back-to-top").click(function(e) {
+    e.preventDefault();
+      $('html,body').animate({
+          scrollTop: 0},
+          'slow');
+  });
+
+  $("#what-we-do-link").click(function(e) {
+    e.preventDefault();
+      $('html,body').animate({
+          scrollTop: $(".what-we-do-wrapper").offset().top - 100},
+          'slow');
+  });
+});
