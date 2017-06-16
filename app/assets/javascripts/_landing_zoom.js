ff0b2e74188aeb21510c0e0278163118cc36a218
Clarissa Knox <knox.ck@gmail.com>
2017-06-16 10:26:03 +0100

    now saved oops

--- app/assets/javascripts/_landing_zoom.js
+++ app/assets/javascripts/_landing_zoom.js
@@ -10,9 +10,9 @@
 $(document).ready(function() {
    $('#header-page-down').click( function (e) { 
 	e.preventDefault();
-	Var height = jQuery(window).height(); // GET PAGE HEIGHT
-	height = height + $(‘.navbar-addcap’).height(); // GET HEIGHT OF NAV BAR AND PAGE HEIGHT
-       $( '.blue-value-wrapper').attr( 'style', 'margin-top: -100px'+ height +'px!important;' ); 
+	Var height = jQuery(window).height(); /
+	height = height + $(‘.navbar-addcap’).height(); 
+       $( '.blue-value-wrapper').attr( 'style', 'margin-top: -'+ height +'px!important;' ); 
        $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {}); 
     });
 });
