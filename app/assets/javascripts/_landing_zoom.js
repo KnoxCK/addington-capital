bfff16aa25a6782c89e8ee67d66e06bb13581e90
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-18 10:01:03 +0100

    landing zoon refactor

--- app/assets/javascripts/_landing_zoom.js
+++ app/assets/javascripts/_landing_zoom.js
@@ -5,13 +5,12 @@ $( document ).ready( function() {
           scrollTop: $(".what-we-do-wrapper").offset().top - 100},
           'slow');
   });
-  
-$('#header-page-down').click( function (e) { 
+
+$('#header-page-down').click( function (e) {
 	e.preventDefault();
-	var height = jQuery(window).height(); 
-	height = height + $('.navbar-addcap').height() - 25; 
-       $( '.blue-value-wrapper').attr( 'style', 'margin-top: -'+ height +'px!important;' ); 
-       $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {}); 
+	var height = jQuery(window).height() + $('.navbar-addcap').height() - 25;
+       $( '.blue-value-wrapper').attr( 'style', 'margin-top: -'+ height +'px!important;' );
+       $('#home-main').animate({ "top": -jQuery(window).height() }, 200, function () {});
     });
 
 });
