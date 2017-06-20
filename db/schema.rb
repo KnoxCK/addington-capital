ffd6709ebe9cd5e06d41bc5fa3e6abcbc748c48f
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-20 13:30:17 +0100

    case study and category models

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 20170620115921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "case_studies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "summary"
+    t.text     "description"
+    t.string   "thumbnail"
+    t.string   "photo"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_case_studies_on_category_id", using: :btree
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -43,18 +55,6 @@ ActiveRecord::Schema.define(version: 20170620115921) do
     t.datetime "updated_at",      null: false
   end
 
-  create_table "table_case_studies", force: :cascade do |t|
-    t.string   "title"
-    t.text     "summary"
-    t.text     "description"
-    t.string   "thumbnail"
-    t.string   "photo"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_table_case_studies_on_category_id", using: :btree
-  end
-
   create_table "transactions", force: :cascade do |t|
     t.integer  "value"
     t.datetime "created_at", null: false
@@ -79,5 +79,5 @@ ActiveRecord::Schema.define(version: 20170620115921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "table_case_studies", "categories"
+  add_foreign_key "case_studies", "categories"
 end
