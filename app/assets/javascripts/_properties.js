5d709cc838fe5414f7ee1e0fefb5344b8abf9ca0
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-21 18:37:31 +0100

    refactor

--- app/assets/javascripts/_properties.js
+++ app/assets/javascripts/_properties.js
@@ -1,3 +1,34 @@
+//Small image linking to large image
+$(".case-studies-box").click(function(e) {
+  e.preventDefault();
+  $(".case-study-lg").removeClass("active");
+  $($(this).data('category')).removeClass('hidden');
+  $($(this).data('target')).addClass("active");
+  $('html,body').animate({
+      scrollTop: $(".case-study-detailed").offset().top - 100},
+      'slow');
+});
+
+//Right Arrow
+$('.prop-arrow-right').click(function(e){
+  e.preventDefault();
+  var active = $('.case-study-lg.active')
+  if (active.next().is("div")) {
+    active.removeClass('active');
+    active.next().addClass('active');
+  }
+});
+
+//Left Arrow
+$('.prop-arrow-left').click(function(e){
+  e.preventDefault();
+  var active = $('.case-study-lg.active')
+  if (active.prev().is("div")) {
+    active.removeClass('active');
+    active.prev().addClass('active');
+  }
+});
+
 // Case study tabs
 $('.retail-tab').click(function(e){
   e.preventDefault();
@@ -22,93 +53,3 @@ $('.offices-tab').click(function(e){
   $('.retail-case-studies').addClass('hidden');
   $('.offices-case-studies').removeClass('hidden');
 });
-
-
-//Small image linking to large image
-$(".case-studies-box").click(function(e) {
-  e.preventDefault();
-  $(".case-study-lg").removeClass("active");
-  $($(this).data('category')).removeClass('hidden');
-  $($(this).data('target')).addClass("active");
-  $('html,body').animate({
-      scrollTop: $(".case-study-detailed").offset().top - 100},
-      'slow');
-});
-
-
-//Right Arrow
-$('.prop-arrow-right').click(function(e){
-  e.preventDefault();
-  var active = $('.case-study-lg.active')
-  if (active.next().is("div")) {
-    active.removeClass('active');
-    active.next().addClass('active');
-  }
-});
-
-//Right Arrow
-$('.prop-arrow-left').click(function(e){
-  e.preventDefault();
-  var active = $('.case-study-lg.active')
-  if (active.prev().is("div")) {
-    active.removeClass('active');
-    active.prev().addClass('active');
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#property-two").click(function(e) {
-//     e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $(".property-wrapper").offset().top - 100},
-//           'slow');
-//       $(".harmsworth").addClass("hidden");
-//   	  $(".randall").removeClass("hidden");
-// });
-// $("#property-three").click(function(e) {
-//     e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $(".property-wrapper").offset().top - 100},
-//           'slow');
-//       $(".harmsworth").removeClass("hidden");
-//   	  $(".randall").addClass("hidden");
-// });
-// $("#property-four").click(function(e) {
-//     e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $(".property-wrapper").offset().top - 100},
-//           'slow');
-//       $(".harmsworth").addClass("hidden");
-//   	  $(".randall").removeClass("hidden");
-// });
-// $("#property-five").click(function(e) {
-//     e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $(".property-wrapper").offset().top - 100},
-//           'slow');
-//       $(".harmsworth").removeClass("hidden");
-//   	  $(".randall").addClass("hidden");
-// });
-// $("#property-six").click(function(e) {
-//     e.preventDefault();
-//       $('html,body').animate({
-//           scrollTop: $(".property-wrapper").offset().top - 100},
-//           'slow');
-//       $(".harmsworth").addClass("hidden");
-//   	  $(".randall").removeClass("hidden");
-// });
-
-
-
-
