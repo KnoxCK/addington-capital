95f2d6de3d43b7ea7a6d8113ac1615d7b292d7c6
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-22 21:05:43 +0100

    what we do to case studies linking working sexy

--- app/assets/javascripts/_what_we_do.js
+++ app/assets/javascripts/_what_we_do.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
     // $('.case-studies-tabs '+hash+'').addClass('properties-button-active');
     $('.retail-case-studies').addClass('hidden');
     $(''+hash+'').removeClass('hidden');
-    // $(".residential-page").removeClass("hidden-sm hidden-md hidden-lg hidden-xl");
+    $('html,body').animate({
+      scrollTop: $(''+hash+'').offset().top - 173});
+      // $(".residential-page").removeClass("hidden-sm hidden-md hidden-lg hidden-xl");
   }
 
-  $('.prop-arrow-right').click(function(e){
+  $('.about-arrow-right').click(function(e){
     e.preventDefault();
     var active = $('.about.active')
     if (active.next().is("div")) {
@@ -16,7 +18,7 @@ $(document).ready(function(){
     }
   });
 
-  $('.prop-arrow-left').click(function(e){
+  $('.about-arrow-left').click(function(e){
     e.preventDefault();
     var active = $('.about.active')
     if (active.prev().is("div")) {
