c1419afe52364300e4a2a27b9435a74d0f6e939d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-22 17:41:51 +0100

    what we do arrows

--- app/assets/javascripts/_what_we_do.js
+++ app/assets/javascripts/_what_we_do.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
   var hash = window.location.hash
   if (hash) {
-    $('.properties-buttons '+hash+'').addClass('properties-button-active');
-    $(".commercial-page").addClass("hidden");
-    $(".residential-page").removeClass("hidden-sm hidden-md hidden-lg hidden-xl");
+    // $('.case-studies-tabs '+hash+'').addClass('properties-button-active');
+    $('.retail-case-studies').addClass('hidden');
+    $(''+hash+'').removeClass('hidden');
+    // $(".residential-page").removeClass("hidden-sm hidden-md hidden-lg hidden-xl");
   }
+
+  $('.prop-arrow-right').click(function(e){
+    e.preventDefault();
+    var active = $('.about.active')
+    if (active.next().is("div")) {
+      active.removeClass('active');
+      active.next().addClass('active');
+    }
+  });
+
+  $('.prop-arrow-left').click(function(e){
+    e.preventDefault();
+    var active = $('.about.active')
+    if (active.prev().is("div")) {
+      active.removeClass('active');
+      active.prev().addClass('active');
+    }
+  });
 });
 
