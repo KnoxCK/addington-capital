190f5f56f25b6648394f1896289ef6132f26495d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-23 13:59:57 +0100

    working perfect for first one but not the rest - and no idea why

--- app/assets/javascripts/press_media.js
+++ app/assets/javascripts/press_media.js
@@ -1,14 +1,3 @@
-$(document).on('click', '#news-read-more', function(e) {
-  $(".full-news").removeClass("hidden");
-	$(".news-brief").addClass("hidden");
-	e.preventDefault();
-});
-$(document).on('click', '#news-close', function(e) {
-  $(".full-news").addClass("hidden");
-	$(".news-brief").removeClass("hidden");
-	e.preventDefault();
-});
-
 $(document).ready(function(){
 
   $('.press-tabs a').click(function(e){
@@ -33,4 +22,16 @@ $(document).ready(function(){
     e.preventDefault();
     $($(this).data('target')).toggleClass('hidden');
   });
+
+  $('#news-read-more').click(function(e) {
+    e.preventDefault();
+    $(this).parent().parent().addClass('hidden');
+    $($(this).data('target')).removeClass('hidden');
+  })
+
+  $('#news-close').click(function(e) {
+    e.preventDefault();
+    $(this).parent().parent().addClass('hidden');
+    $($(this).data('target')).removeClass('hidden');
+  })
 });
