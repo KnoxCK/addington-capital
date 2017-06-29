a2a2ac9e98676a58a2606aa447a4539baf6156d0
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-29 10:28:26 +0100

    press body fix

---
+++ db/migrate/20170629091440_create_management_teams.rb
@@ -0,0 +1,14 @@
+class CreateManagementTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :management_teams do |t|
+      t.string :name
+      t.string :position
+      t.text :description
+      t.string :vcard
+      t.string :twitter_url
+      t.string :linkedin_url
+      t.string :facebook_url
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623151702) do
+ActiveRecord::Schema.define(version: 20170629091440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 20170623151702) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "management_teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "position"
+    t.text     "description"
+    t.string   "vcard"
+    t.string   "twitter_url"
+    t.string   "linkedin_url"
+    t.string   "facebook_url"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "press_releases", force: :cascade do |t|
