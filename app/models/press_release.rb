42201e86cfda237e468be98919a3de9684db0b7a
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-17 15:25:04 +0100

    fix for no date

--- app/models/press_release.rb
+++ app/models/press_release.rb
@@ -2,6 +2,6 @@ class PressRelease < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   def formatted_date
-    self.date.strftime('%d %b, %Y')
+    self.date.strftime('%d %b, %Y') if self.date
   end
 end
