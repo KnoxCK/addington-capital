1f13eb8cc6ed5b06e845f8c42f9bf78860a252ba
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-18 16:39:30 +0100

    priority validation

--- app/models/case_study.rb
+++ app/models/case_study.rb
@@ -1,4 +1,6 @@
 class CaseStudy < ApplicationRecord
   belongs_to :category
   mount_uploader :photo, PhotoUploader
+  validates_presence_of :title
+  validates :priority, inclusion: { in: [1, 2, 3, 4, 5, 6] }
 end
